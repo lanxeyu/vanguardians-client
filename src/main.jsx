0ccd50e7f6b28b2218bb81f62ff7e48c090801bb
DuncanCaplan <duncan.caplan@outlook.com>
2023-09-26 14:57:23 +0100

    signup axios

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {BrowserRouter as Router} from 'react-router-dom'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { AuthProvider } from "./context/AuthProvider.jsx";
+import App from "./App.jsx";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <AuthProvider>
+            <Router>
+                <App />
+            </Router>
+        </AuthProvider>
+    </React.StrictMode>
+);
