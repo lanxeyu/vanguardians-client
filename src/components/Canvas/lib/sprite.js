2a7a99b8a001c37ad8eb33d1059a265b011be95e
DuncanCaplan <duncan.caplan@outlook.com>
2023-09-29 14:32:11 +0100

    animation setup

--- src/components/Canvas/lib/sprite.js
+++ src/components/Canvas/lib/sprite.js
@@ -8,7 +8,7 @@ class Sprite {
 }
 
 class Img extends Sprite {
-    constructor(x, y, imageSrc, scale = 1) {
+    constructor(x, y, imageSrc, scale = 1, framesMax = 1) {
         super();
         this.position = { x, y };
         this.width = 50;
@@ -16,19 +16,37 @@ class Img extends Sprite {
         this.image = new Image();
         this.image.src = imageSrc;
         this.scale = scale;
+        this.framesMax = framesMax;
+        this.framesCurrent = 0;
+        this.framesElapsed = 0;
+        this.framesHold = 5;
     }
 
     draw(context) {
         context.drawImage(
             this.image,
+            this.framesCurrent * (this.image.width / this.framesMax),
+            0,
+            this.image.width / this.framesMax,
+            this.image.height,
             this.position.x,
             this.position.y,
-            this.image.width * this.scale,
+            (this.image.width / this.framesMax) * this.scale,
             this.image.height * this.scale
         );
     }
 
-    update() {}
+    update() {
+        this.framesElapsed++;
+
+        if (this.framesElapsed % this.framesHold === 0) {
+            if (this.framesCurrent < this.framesMax - 1) {
+                this.framesCurrent++;
+            } else {
+                this.framesCurrent = 0;
+            }
+        }
+    }
 }
 
 // --------------------  CHARACTER CLASS - Parent of Guardian & Enemy classes  --------------------
