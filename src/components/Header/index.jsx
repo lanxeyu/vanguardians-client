08d6cf2e25e455b8686f9d8a0067c1d7a0e1375c
DuncanCaplan <duncan.caplan@outlook.com>
2023-10-03 12:52:05 +0100

    logout progress

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,23 +1,39 @@
-import React from 'react';
-import { NavLink, Outlet } from 'react-router-dom';
-const styles = ({ isActive }) => ({ textDecoration: isActive ? 'underline' : 'none'});
-import './index.css'
+import React from "react";
+import { NavLink, Outlet } from "react-router-dom";
+const styles = ({ isActive }) => ({ textDecoration: isActive ? "underline" : "none" });
+import "./index.css";
 
-export default function Header () {
+export default function Header() {
+    const isLoggedIn = JSON.stringify(localStorage.getItem("token"));
     return (
-      <>
-        <header>
-            <NavLink id='nav-title' to="/">VanGuardians</NavLink>
-          <nav>
-            <NavLink to="/" style={styles}>Home</NavLink>
-            <NavLink to="/leaderboard" style={styles}>Leaderboard</NavLink>
-            <NavLink to="/guardians" style={styles}>Guardians</NavLink>
-            <NavLink to="/game" style={styles}>Game</NavLink>
-            <NavLink to="/login" style={styles}>Login</NavLink>
-            <NavLink to="/signup" style={styles}>Sign Up</NavLink>
-          </nav>
-        </header>
-        <Outlet />
-      </>
-    )
+        <>
+            <header>
+                <NavLink id="nav-title" to="/">
+                    VanGuardians
+                </NavLink>
+                <nav>
+                    <NavLink to="/" style={styles}>
+                        Home
+                    </NavLink>
+                    <NavLink to="/leaderboard" style={styles}>
+                        Leaderboard
+                    </NavLink>
+                    <NavLink to="/guardians" style={styles}>
+                        Guardians
+                    </NavLink>
+                    <NavLink to="/game" style={styles}>
+                        Game
+                    </NavLink>
+                    {!isLoggedIn ? null : (
+                        <NavLink to="/login" style={styles}>
+                            Login
+                        </NavLink>
+                    )}
+
+                    {/* <NavLink to="/signup" style={styles}>Sign Up</NavLink> */}
+                </nav>
+            </header>
+            <Outlet />
+        </>
+    );
 }
