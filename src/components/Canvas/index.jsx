530ff4a6c51c05a4d08525b48e8ce86ac9cd506e
Robbie Stamp <stamprobbie@gmail.com>
2023-10-04 12:51:41 +0100

    adding lightning strike animation to Wizard

--- src/components/Canvas/index.jsx
+++ src/components/Canvas/index.jsx
@@ -68,9 +68,9 @@ const Canvas = () => {
         // spawnDuncan();
         // spawnLanxe();
         // spawnSteph();
-        // spawnRobbie();
+        spawnRobbie();
         // spawnJames();
-        spawnAlex();
+        // spawnAlex();
 
         for (let i = 0; i < guardians.length; i++) {
             new PortraitIcon(guardians[i], 20, canvas.height - 160 - 120, i)
